9e9249117c5dcf0a0b05ef33db5dddd6c32c890b
ry <2834016348@qq.com>
2019-02-21 15:55:38 +0800

    state 的用法

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,8 +1,19 @@
 import React from 'react';
 class Welcome extends React.Component {
-  render() {
-    return <h1>Hello, {this.props.name}</h1>;
-  }
+	constructor(props){
+		super(props)
+		this.state = {
+			date: new Date()
+		}
+	}
+  	render() {
+    	return (
+    		<div>
+    			<h1>Hello, {this.props.name}</h1>
+    			<h2>{this.state.date.toString()}</h2>
+  			</div>
+  		)
+  	}
 }
 
 export default Welcome;
