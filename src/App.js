9acce5487b137110e144f8df0a1cb184a10c28dd
ry <2834016348@qq.com>
2019-02-26 22:08:46 +0800

    删除 localStore 功能

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import 'normalize.css';
 import './reset.css';
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
-import * as localStore from './localStore'
 
 
 
@@ -15,7 +14,7 @@ class App extends Component {
     super(props)
     this.state = {
       newTodo: '',
-      todoList: localStore.load('todoList') || []
+      todoList: []
     }
   }
   render() {
@@ -44,7 +43,6 @@ class App extends Component {
     );
   }
   componentDidUpdate(){
-    localStore.save('todoList', this.state.todoList)
   }
   toggle(e, todo){
     todo.status = todo.status === 'completed' ? '' : 'completed'
