36826c212521e6d987478fb8c0bd311480812c2d
ry <2834016348@qq.com>
2019-02-26 16:21:44 +0800

    新建 leanCloud.js

---
+++ src/leanCloud.js
@@ -0,0 +1,8 @@
+import AV from 'leancloud-storage'
+var APP_ID = 'Fon1CyKd3MjxRQpLkRczEln4-gzGzoHsz';
+var APP_KEY = 'VfgQ2WKGyj0c8CXvj940qnqi';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
