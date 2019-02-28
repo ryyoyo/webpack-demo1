95f072f2da9a051fc2ebaed7b4207bc604c0bdf8
ry <2834016348@qq.com>
2019-02-28 09:23:20 +0800

    点击注册后使用 LeanCloud API 注册

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -6,3 +6,27 @@ AV.init({
   appId: APP_ID,
   appKey: APP_KEY
 });
+
+export default AV
+
+export function signUp(username, password, successFn, errorFn){
+	var user = new AV.User()
+	user.setUsername(username)
+	user.setUserPassword(password)
+	user.signUp().then(function(loginedUser){
+		let user = getUserFromAVUser(loginedUser)
+		successFn.call(null, user)
+	}, function(error){
+		errorFn.call(null, error)
+	})
+	return undefined
+}
+
+function getUserFromAVUser(AVUser){
+	console.log(1)
+	
+	return {
+		id: AVUser.id,
+		...AVUser.attributes
+	}
+}
